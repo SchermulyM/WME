6194a626a0f3cbdde23d44a6c08ce45a8d8e7a05
Martin <martin.volkmann96@gmail.com>
2018-11-04 22:36:35 +0100

    rounded table contents, changed css to hide last table col for responsiveness requirements

--- A1/world_data_to_table.py
+++ A1/world_data_to_table.py
@@ -1,13 +1,26 @@
 import csv
 import jinja2
 
+def prettify(s: str):
+    x = s.strip()
+    try:
+        x = int(x)
+    except ValueError:
+        try:
+            x = float(x)
+            x = round(x, 2)
+        except ValueError:
+            pass
+    return str(x)
+
+
 with open("world_data_v1.csv", encoding="UTF-8") as file:
     reader = csv.reader(file, delimiter=",")
 
     data = []
 
     for row in reader:
-        row = tuple(map(lambda x: x.strip(), row))
+        row = tuple(map(prettify, row))
         data.append(row[:7])
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(""))
