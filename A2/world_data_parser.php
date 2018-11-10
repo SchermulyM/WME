6c8cf6e1e162ca2d85fa90c8133c38f13e60b1b7
Martin <martin.volkmann96@gmail.com>
2018-11-10 18:36:00 +0100

    potential fix for saveXML return value

--- A2/world_data_parser.php
+++ A2/world_data_parser.php
@@ -1,57 +1,65 @@
 <?php
 
-class WorldDataParser{
+class WorldDataParser
+{
 
-	// function parseCSV($csv_path) {
-	// 	$filepath = $csv_path . DIRECTORY_SEPARATOR . 'world_data.csv';
-	// 	$csv_array = array_map('str_getcsv', file($filepath));
-	// 	return $csv_array
-	// }
+    // function parseCSV($csv_path) {
+    // 	$filepath = $csv_path . DIRECTORY_SEPARATOR . 'world_data.csv';
+    // 	$csv_array = array_map('str_getcsv', file($filepath));
+    // 	return $csv_array
+    // }
 
-	function parseCSV($csv_path) {
-		$csv_array = array_map('str_getcsv', file($csv_path));
-		return $csv_array;
-	}
+    function parseCSV($csv_path)
+    {
+        $csv_array = array_map('str_getcsv', file($csv_path));
+        return $csv_array;
+    }
 
-	function saveXML($csv_array) {
-		// go through first array and collect tags
-		$xml_tags_array = array();
-		foreach($csv_array[0] as $line) {
-			$tag = str_replace(' ', '_', trim($line));
-	    $xml_tags_array[] = $tag;
-		}
-		$xml = new DOMDocument("1.0", "UTF-8");
-		$xml->preserveWhiteSpace = false;
-		$xml->formatOutput = true;
-		$xml_countries = $xml -> createElement('Countries');
-		// go through remaining array in array, create xml from every entry.
-		foreach($csv_array as $entry_nr => $entry){
-			if($entry_nr) {
-				$xml_country = $xml -> createElement("Country");
-				foreach($entry as $index => $value){
-					$value = trim($value);
-					$xele = $xml -> createElement($xml_tags_array[$index]);
-					$xele -> nodeValue = $value;
-					$xml_country -> appendChild($xele);
-				}
-				$xml_countries -> appendChild($xml_country);
-			}
-		}
-		$xml -> appendChild($xml_countries);
-		$ret = $xml -> save('world_data.xml');
-		return boolval($ret);
-	}
+    function saveXML($csv_array)
+    {
+        try {
+            // go through first array and collect tags
+            $xml_tags_array = array();
+            foreach ($csv_array[0] as $line) {
+                $tag = str_replace(' ', '_', trim($line));
+                $xml_tags_array[] = $tag;
+            }
+            $xml = new DOMDocument("1.0", "UTF-8");
+            $xml->preserveWhiteSpace = false;
+            $xml->formatOutput = true;
+            $xml_countries = $xml->createElement('Countries');
+            // go through remaining array in array, create xml from every entry.
+            foreach ($csv_array as $entry_nr => $entry) {
+                if ($entry_nr) {
+                    $xml_country = $xml->createElement("Country");
+                    foreach ($entry as $index => $value) {
+                        $value = trim($value);
+                        $xele = $xml->createElement($xml_tags_array[$index]);
+                        $xele->nodeValue = $value;
+                        $xml_country->appendChild($xele);
+                    }
+                    $xml_countries->appendChild($xml_country);
+                }
+            }
+            $xml->appendChild($xml_countries);
+            $ret = $xml->save('world_data.xml');
+            return boolval($ret);
+        } catch (Exception $exception) {
+            return false;
+        }
+    }
 
-	function printXML($xml_file, $xsl_file){
-		$xslDoc = new DOMDocument("1.0", "UTF-8");
-    $xslDoc->load($xsl_file);
+    function printXML($xml_file, $xsl_file)
+    {
+        $xslDoc = new DOMDocument("1.0", "UTF-8");
+        $xslDoc->load($xsl_file);
 
-    $xmlDoc = new DOMDocument("1.0", "UTF-8");
-    $xmlDoc->load($xml_file);
+        $xmlDoc = new DOMDocument("1.0", "UTF-8");
+        $xmlDoc->load($xml_file);
 
-    $proc = new XSLTProcessor();
-    $proc->importStylesheet($xslDoc);
-    $proc = $proc->transformToXML($xmlDoc);
-		return $proc;
-	}
+        $proc = new XSLTProcessor();
+        $proc->importStylesheet($xslDoc);
+        $proc = $proc->transformToXML($xmlDoc);
+        return $proc;
+    }
 }
