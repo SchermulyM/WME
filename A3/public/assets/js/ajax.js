7902ea405108f4539f564032d5d4e2f0f41015fb
Martin <martin.volkmann96@gmail.com>
2018-11-21 05:38:08 +0100

    loading properties via ajax on page ready

--- A3/public/assets/js/ajax.js
+++ A3/public/assets/js/ajax.js
@@ -1,1 +1,15 @@
-alert("No ajax calls implemented ;)");
+$(() => {
+    $.ajax({
+        url: "/properties",
+        success: data => {
+            console.log(data);
+            const optionsHtml = data.map(d => `<option>${d}</option>`).join("");
+            $("#prop_selection").html(optionsHtml);
+        }
+    });
+
+    $("#country_add").submit((event) => {
+        alert("submitted");
+        event.preventDefault();
+    });
+});
