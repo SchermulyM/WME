f24b7b0650865ddcbdd987530c01a07a643ae11a
Martin <martin.volkmann96@gmail.com>
2019-01-19 17:01:42 +0100

    things are placed on the map but performance is still garbage and we need bar charts instead

--- A5/public/assets/js/a5.js
+++ A5/public/assets/js/a5.js
@@ -5,7 +5,7 @@ let scene;
 let camera;
 let renderer;
 
-let material;
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 let meshes;
 
 $(() => {
@@ -13,9 +13,25 @@ $(() => {
     initMappa();
     initThree();
     myMap.onChange(() => {
-        console.log("map changed");
+        while (scene.children.length > 0) {
+            scene.remove(scene.children[0]);
+        }
+        for (const d of data ? data : []) {
+            const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+            const mesh = new THREE.Mesh(geometry, material);
+            const pos = myMap.latLngToPixel(d.gps_lat, d.gps_long);
+            const vector = new THREE.Vector3();
+            vector.set((pos.x / canvas.width) * 2 - 1, -(pos.y / canvas.height) * 2 + 1, 0.5);
+            vector.unproject(camera);
+            const dir = vector.sub(camera.position).normalize();
+            const distance = -camera.position.z / dir.z;
+            const newPos = camera.position.clone().add(dir.multiplyScalar(distance));
+            mesh.position.set(newPos.x, newPos.y, newPos.z);
+            scene.add(mesh);
+        }
     });
     updateData();
+    animationLoop();
 });
 
 function initMappa() {
@@ -34,8 +50,10 @@ function initMappa() {
 
 function initThree() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
     renderer = new THREE.WebGLRenderer({alpha: true, canvas: canvas});
+    camera.position.z = 300;
+    scene.add(camera);
     renderer.setSize(canvas.width, canvas.height);
 }
 
@@ -52,4 +70,9 @@ function updateData(callback) {
             if (callback) callback();
         }
     });
+}
+
+function animationLoop() {
+    requestAnimationFrame(animationLoop);
+    renderer.render(scene, camera);
 }
